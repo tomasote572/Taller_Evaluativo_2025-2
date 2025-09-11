e7c8a56ace59d2854cbe45ecdb3145b142ed16e6
tomasote572 <tomas.ramirez@mail.escuelaing.edu.co>
2025-09-10 20:53:49 -0500

    creacion: Tomas Ramirez: se crearon los ficheros y corrigio dependencias

---
+++ src/main/java/edu/dosw/lab/Taller_Evaluativo_2025_2/TallerEvaluativo20252Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.Taller_Evaluativo_2025_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerEvaluativo20252Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerEvaluativo20252Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/lab/Taller_Evaluativo_2025_2/TallerEvaluativo20252ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.Taller_Evaluativo_2025_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerEvaluativo20252ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
